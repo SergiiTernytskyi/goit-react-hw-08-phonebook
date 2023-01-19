afdc5dc817990867a444de232383da5681c91551
Serhii-Ter <serhij86@gmail.com>
2023-01-19 23:36:33 +0200

    Update filterSlice.js

--- src/redux/filter/filterSlice.js
+++ src/redux/filter/filterSlice.js
@@ -1,4 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { logOut } from 'redux/auth/operations';
+
+const logOutFulfilledReducer = state => {
+  return (state = '');
+};
 
 const filterSlice = createSlice({
   name: 'filter',
@@ -8,6 +13,8 @@ const filterSlice = createSlice({
       return (state = action.payload);
     },
   },
+  extraReducers: builder =>
+    builder.addCase(logOut.fulfilled, logOutFulfilledReducer),
 });
 
 export const { filterContacts } = filterSlice.actions;
